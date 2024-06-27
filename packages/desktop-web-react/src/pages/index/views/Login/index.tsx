0176ce6a20e0fcbce9a9aec208eaa36e8b971fa3
suey <yanxu@xiaoduotech.com>
2024-06-27 19:05:22 +0800

    feat: 适配了登录窗口大小

--- packages/desktop-web-react/src/pages/index/views/Login/index.tsx
+++ packages/desktop-web-react/src/pages/index/views/Login/index.tsx
@@ -2,6 +2,8 @@
 import { FullSize } from '@/styled';
 import { workbenchesRoute } from '@pages/index/router';
 import { useLocation, useNavigate, useNavigation } from 'react-router-dom';
+import { useFadeIn } from '@hooks/index';
+import { windowEnableFullScreen, windowResizeAble, windowSetSize } from '@/actions';
 
 import Header from '@components/Header';
 
@@ -9,6 +11,13 @@ import Header from '@components/Header';
 export default function Login() {
   // const navigate = useNavigate();
   // const navigation = useNavigation();
+
+  useFadeIn(async () => {
+
+    await windowSetSize({ width: 800, height: 500 });
+
+    await windowResizeAble({ able: false });
+  });
 
 
   return <FullSize>
