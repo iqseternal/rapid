c098771f3b049f3642733f1cf95e93627c84b579
suey <yanxu@xiaoduotech.com>
2024-12-03 15:46:40 +0800

    feat: 添加配色文件

---
+++ apps/scripts/src/index.tsx
@@ -0,0 +1,58 @@
+import { Printer, printClear } from '@suey/printer';
+
+Printer.print('Hello');
+
+let i = 0, j = true;
+
+if (i < 0) console.log(`${i}`);
+
+for (let i = 0;i < 100;i ++) {
+  if (i === 0) continue;
+  break;
+}
+
+abstract class A {
+  public static readonly name: string = 'a';
+
+  abstract run(): void;
+}
+
+class B extends A {
+  a = 1;
+
+  override run(): void {
+    this.a = parseInt(A.name);
+  }
+}
+
+const b = new B();
+
+interface Props {
+  className?: string;
+}
+
+declare function Component(props: Props): JSX.Element;
+
+declare const bc: string;
+
+namespace React {
+  export interface ReactElement {
+    type: string;
+  }
+}
+
+type P = Partial<React.ReactElement>;
+
+enum Platform {
+  Windows = 1
+}
+
+window.name;
+
+const Home = <div>
+  <Component
+    className={'sdad'}
+  >
+
+  </Component>
+</div>
