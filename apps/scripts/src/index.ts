c6d85a9680665ce87d418924dfcfeea2bd14916e
suey <yanxu@xiaoduotech.com>
2024-12-31 20:04:30 +0800

    fix: 删除不必要的工作测试代码

--- apps/scripts/src/index.ts
+++ apps/scripts/src/index.ts
@@ -1,14 +1,7 @@
-import { forge, bcrypt } from '@rapid/libs';
-
-const KEY = `
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCS56315ZowpncDwXZhXb4j6BHK
-Gor8OZe+06s73C4H5b/Iyy/vbAzFFWsfL1y04AnNDkZ9swoFWnmYjon1XGUTvN3Z
-BuLWKW9UjDCcTcno4MEhP1qrk3X0iiBeu09aJdiWogjbN4kaBq/KcKRQ5kvXdcTP
-hvcDZhJI/YiPgbRW9wIDAQAB
------END PUBLIC KEY-----`;
-
-const publicKey = forge.pki.publicKeyFromPem(KEY);
 
 
 
+
+console.log(
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].slice(5, 10)
+)
