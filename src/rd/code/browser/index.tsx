59d8080d8ac369330f6715a93ab9db10297e773a
sueyyan <sueyeternal@163.com>
2025-03-14 00:45:34 +0800

    feat: commit

--- src/rd/code/browser/index.tsx
+++ src/rd/code/browser/index.tsx
@@ -1,20 +1,47 @@
 import './inject';
 
-import { useState, StrictMode } from 'react';
+import { useState, StrictMode, memo, useLayoutEffect } from 'react';
 import { Ansi } from '@rapid/libs';
+import { RdSKin } from './skin';
 
 import ReactDOM from 'react-dom/client';
-
-import RdAppWrapper from './app';
+import RdApp from './app';
 import RdThemeExtension from './plats/extensions/RdThemeExtension';
 
 import '@/scss/index.scss';
 
 import './tailwind.css';
 
-rApp.extension.registerExtension(RdThemeExtension);
 
 // ===========================================================================================
+rApp.extension.registerExtension(RdThemeExtension);
+
+/**
+ * App component, 这里做各种功能的插入：例如 插件等等
+ */
+const RdAppWrapper = memo(() => {
+  rApp.extension.useExtensions();
+
+  const themePayloadTransformers = rApp.metadata.useMetadata('functional.theme.variables.transformer');
+
+  useLayoutEffect(() => {
+    if (!themePayloadTransformers) return;
+
+    let declaration = RdSKin.toCssVariablesDeclaration();
+    themePayloadTransformers.forEach(transform => {
+      declaration = transform(declaration);
+    })
+
+    RdSKin.install(declaration);
+
+    return () => {
+      RdSKin.uninstall();
+    }
+  }, [themePayloadTransformers]);
+
+  return (<RdApp />)
+})
+
 const rootContainer = document.getElementById('root');
 
 if (rootContainer) {
