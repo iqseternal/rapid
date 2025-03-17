8cc8e3c6457e463cf99a5c67ca87f16e5330be02
suey <yanxu@xiaoduotech.com>
2025-03-17 20:03:35 +0800

    feat: 编写插件打包模板

--- cli/rxc/template/rspack.config.ts
+++ cli/rxc/template/rspack.config.ts
@@ -1,6 +1,110 @@
 import { defineConfig } from '@rspack/cli';
+import type { RuleSetRule } from '@rspack/core';
+import { resolve } from 'path';
+import { SwcJsMinimizerRspackPlugin } from '@rspack/core';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly NODE_ENV: 'development' | 'production';
+    }
+  }
+}
+
+const supportImportRaw: RuleSetRule = {
+  resourceQuery: /(\?raw$)|(\.(png|jpe?g|gif|ico)$)/,
+  exclude: [/node_modules/],
+  type: 'asset/resource'
+} as const;
+
+const supportTypescript: RuleSetRule = {
+  test: /\.(c)?[tj]sx?$/,
+  loader: 'builtin:swc-loader',
+  options: {
+    jsc: {
+      loose: true,
+      parser: {
+        syntax: 'typescript',
+        decorators: true,
+
+      },
+      transform: {
+        legacyDecorator: true,
+        decoratorMetadata: true
+      },
+    },
+  },
+  type: 'javascript/auto',
+} as const;
+
+export default defineConfig(() => {
 
 
-export default defineConfig({
+  return {
+    entry: './src/index.tsx',
+    mode: process.env.NODE_ENV,
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+      asyncChunks: true,
+      chunkFilename: '[name].js',
+      iife: true,
+    },
+    plugins: [
 
+    ],
+    module: {
+      rules: [
+        supportImportRaw,
+        supportTypescript
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    cache: true,
+
+    watch: true,
+    watchOptions: {
+
+
+    },
+    devServer: {
+
+    },
+    optimization: {
+
+      chunkIds: 'named' as const,
+      moduleIds: 'named' as const,
+      sideEffects: 'flag' as const,
+
+      minimize: true,
+      minimizer: [
+        new SwcJsMinimizerRspackPlugin({
+          minimizerOptions: {
+            minify: true,
+            mangle: false,
+            compress: false,
+
+            format: {
+              keep_quoted_props: true,
+              beautify: true,
+              braces: true,
+              indent_level: 2,
+              semicolons: true,
+              ecma: 2016
+            },
+            module: true,
+          }
+        })
+      ],
+      usedExports: true,
+      providedExports: true,
+      mangleExports: 'deterministic' as const,
+      innerGraph: true,
+      concatenateModules: true,
+    },
+    externals: ['react']
+  }
 });
