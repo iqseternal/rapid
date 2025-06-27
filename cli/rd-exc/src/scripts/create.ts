3c5891cecd6ee06b48c60178e07099a5ad74b10d
suey <yanxu@xiaoduotech.com>
2025-06-27 16:48:44 +0800

    fix: cli

--- cli/rd-exc/src/scripts/create.ts
+++ cli/rd-exc/src/scripts/create.ts
@@ -1,4 +1,6 @@
-
+import * as fs from 'fs';
+import * as path from 'path';
+import * as fse from 'fs-extra';
 
 export interface CreateActionOptions {
 
@@ -6,7 +8,16 @@ export interface CreateActionOptions {
 
 export function createAction(extName: string, options: CreateActionOptions) {
 
+  const cwd = process.cwd();
 
+  const dir = path.join(cwd, extName);
 
+  if (fs.existsSync(dir) && fs.statSync(dir).isDirectory()) {
+    console.error(`目录 ${dir} 已存在`);
+    process.exit(1);
+  }
 
+  fs.mkdirSync(dir);
+
+  fse.copy(path.join(__dirname, '../../templates/online'), dir);
 }
